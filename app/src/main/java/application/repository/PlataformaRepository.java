4c923421d27513d6fe16fc51f1f76f479d470bdd
offrafell <142051877+offrafell@users.noreply.github.com>
2024-08-26 14:05:55 -0300

    Initial commit

---
+++ app/src/main/java/application/repository/PlataformaRepository.java
@@ -0,0 +1,6 @@
+package application.repository;
+import org.springframework.data.repository.CrudRepository;
+import application.model.Plataforma;
+
+public interface PlataformaRepository extends CrudRepository<Plataforma, Long> {
+}
